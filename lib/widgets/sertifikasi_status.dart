5277bcb86cf53d2b7e9e7734c3341bbfcd2a52e5
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2024-10-16 13:12:19 +0700

    Page Status Sertifikasi

--- lib/widgets/sertifikasi_status.dart
+++ lib/widgets/sertifikasi_status.dart
@@ -5,8 +5,67 @@ class SertifikasiStatus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Tab Status"),
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              fillColor: const Color.fromARGB(145, 255, 249, 249),
+              filled: true,
+            ),
+          ),
+          const SizedBox(
+              height:
+                  20), // Spasi antara search bar dan tombol Pengajuan & Upload
+
+          // Button Pengajuan & Upload
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi ketika tombol "Pengajuan" ditekan
+                  print("Pengajuan ditekan");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(
+                      255, 55, 94, 151), // Warna tombol biru
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 15), // Padding tombol
+                ),
+                child: const Text(
+                  'Pengajuan',
+                  style: TextStyle(color: Colors.white), // Warna teks putih
+                ),
+              ),
+              const SizedBox(
+                  width: 10), // Jarak antar tombol Pengajuan dan Upload
+
+              ElevatedButton(
+                onPressed: () {
+                  // Aksi ketika tombol "Upload" ditekan
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(
+                      255, 239, 84, 40), // Warna tombol oranye
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 15), // Padding tombol
+                ),
+                child: const Text(
+                  'Upload',
+                  style: TextStyle(color: Colors.white), // Warna teks putih
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
