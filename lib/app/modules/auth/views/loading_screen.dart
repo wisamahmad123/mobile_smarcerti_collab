0400bc03119ecb716dd200acd7cb4e7336fcc277
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-11-13 13:27:12 +0700

    Edit login

--- lib/app/modules/auth/views/loading_screen.dart
+++ lib/app/modules/auth/views/loading_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_smarcerti/app/hooks/use_auth.dart';
-import 'login_page.dart';  // Import halaman login
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -17,17 +16,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-   pref();
+    pref();
   }
 
   void pref() async {
-      Future.delayed(const Duration(seconds: 3), () async {
-    if(await _auth.isLoggedIn()){
-      
-      Get.offAllNamed('/login');
-    } else {
-      // Get.offAllNamed('/home');
-    }
+    Future.delayed(const Duration(seconds: 3), () async {
+      if (await _auth.isLoggedIn()) {
+        // Jika sudah login, arahkan ke halaman home
+        Get.offAllNamed('/home');
+      } else {
+        // Jika belum login, arahkan ke halaman login
+        Get.offAllNamed('/login');
+      }
     });
   }
 
@@ -39,39 +39,35 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Display logo image
             Container(
-              width: 150,  
+              width: 150,
               height: 150,
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("assets/images/logo.png"),  
+                  image: AssetImage("assets/images/logo.png"),
                   fit: BoxFit.contain,
                 ),
               ),
             ),
-            const SizedBox(height: 1),
+            const SizedBox(height: 10),
             const Text(
               'SMARTCERTI',
               style: TextStyle(
-                fontFamily: 'RacingSansOne',  
+                fontFamily: 'RacingSansOne',
                 fontSize: 54,
                 fontWeight: FontWeight.bold,
-                color: Color(0xFFEF5428),  
+                color: Color(0xFFEF5428),
               ),
             ),
-            Transform.translate(
-            offset: const Offset(0, -15),  // Menggeser teks ke atas
-            child: const Text(
+            const SizedBox(height: 15),
+            const Text(
               'Certification Management for JTI',
               style: TextStyle(
-                fontFamily: 'RacingSansOne', 
+                fontFamily: 'RacingSansOne',
                 fontSize: 18,
                 color: Color(0xFF949494),
               ),
             ),
-          ),
-
           ],
         ),
       ),
