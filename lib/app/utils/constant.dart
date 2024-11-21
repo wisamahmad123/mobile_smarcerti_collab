7a27845815661f5c2c497e645fe790a7792aa7aa
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-11-21 22:33:33 +0700

    fix listpelatihandosen db terbaru pakai controller pimpinanpelatihandosen

--- lib/app/utils/constant.dart
+++ lib/app/utils/constant.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.66.67:8000/api/';
+  static const String baseUrl = 'http://192.168.0.100:8000/api/';
 
   static const int connectionTimeout = 5000; // 5 seconds
   static const int receiveTimeout = 4000; // 3 seconds
