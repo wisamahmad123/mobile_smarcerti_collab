a2141e396fb9f82f4dbe3f2f1c6253164daf4cb7
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-11-25 20:51:37 +0700

    list sertifikasi dosen

--- lib/app/modules/list_pelatihan_sertifikasi/views/sertifikasi/detail_page/list_sertifikas_detail_page.dart
+++ lib/app/modules/list_pelatihan_sertifikasi/views/sertifikasi/detail_page/list_sertifikas_detail_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_smarcerti/app/data/models/sertifikasi_model.dart';
 import 'package:mobile_smarcerti/widgets/app_bar_custom.dart';
 import 'package:mobile_smarcerti/app/modules/list_pelatihan_sertifikasi/views/sertifikasi/detail_page/list_sertifikasi_detail_body.dart';
 import 'package:mobile_smarcerti/widgets/pimpinan_bottom_nav_bar.dart';
 
-
 class ListSertifikasDetailPage extends StatelessWidget {
-  const ListSertifikasDetailPage({super.key});
+  final Sertifikasi sertifikasiDetail;
+  ListSertifikasDetailPage({
+    super.key,
+    required this.sertifikasiDetail});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarCustom(title: 'Detail Sertifikasi',),
-      body: ListSertifikasiDetailBody(),
+    return Scaffold(
+      appBar: const AppBarCustom(
+        title: 'Detail Sertifikasi',
+      ),
+      body: ListSertifikasiDetailBody(sertifikasiDetail: sertifikasiDetail),
       bottomNavigationBar: PimpinanBottomNavBar(currentIndex: -1),
     );
   }
--- lib/services/list_pelatihan_sertifikasi_service.dart
+++ lib/services/list_pelatihan_sertifikasi_service.dart
@@ -1,7 +1,7 @@
-import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:mobile_smarcerti/app/data/models/pelatihan_model.dart';
+import 'package:mobile_smarcerti/app/data/models/sertifikasi_model.dart';
 import 'package:mobile_smarcerti/app/utils/constant.dart';
 import 'package:mobile_smarcerti/services/api_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -26,30 +26,7 @@ class ListPelatihanSertifikasiService {
     return token;
   }
 
-  /// Fungsi untuk menyimpan response JSON ke model Pelatihan
-  Pelatihan saveJsonToPelatihanModel(String responseJson) {
-    try {
-      // Decode JSON string menjadi Map
-      final Map<String, dynamic> jsonData = json.decode(responseJson);
-
-      // Validasi apakah struktur data sesuai
-      if (jsonData.containsKey('success') &&
-          jsonData.containsKey('message') &&
-          jsonData.containsKey('data')) {
-        // Parsing ke model Pelatihan
-        final pelatihanModel = Pelatihan.fromJson(jsonData);
-        print('Data berhasil disimpan ke model Pelatihan');
-        return pelatihanModel;
-      } else {
-        throw Exception('JSON tidak sesuai dengan struktur model Pelatihan');
-      }
-    } catch (e) {
-      print('Error parsing JSON to Pelatihan model: $e');
-      throw Exception('Gagal menyimpan data ke model Pelatihan: $e');
-    }
-  }
-
-  // Fungsi untuk mendapatkan data pelatihan
+  // Fungsi untuk mendapatkan data pelatihan dosen
   Future<List<Pelatihan>> getPelatihans() async {
     final token = await getToken(); // Ambil token dari helper atau auth service
     if (token == null) throw Exception("Token not found");
@@ -80,6 +57,40 @@ class ListPelatihanSertifikasiService {
     } catch (e) {
       print('Error fetching pelatihans: $e');
       throw Exception('Error fetching pelatihans: $e');
+    }
+  }
+
+  // Fungsi untuk mendapatkan data sertifikasi dosen
+  Future<List<Sertifikasi>> getSertifikasis() async {
+    final token = await getToken(); // Ambil token dari helper atau auth service
+    if (token == null) throw Exception("Token not found");
+
+    try {
+      final response = await _dio.get(
+        '${ApiConstants.baseUrl}pimpinanSertifikasis',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        // Mengakses data yang berada dalam pelatihans -> data
+        print(response.data);
+        var json = response.data;
+        final parsed = json['data'].cast<Map<String, dynamic>>();
+        print("parsed data : $parsed");
+        return parsed
+            .map<Sertifikasi>((json) => Sertifikasi.fromJson(json))
+            .toList();
+      } else {
+        throw Exception('Failed to load sertifikasis');
+      }
+    } catch (e) {
+      print('Error fetching sertifikasis: $e');
+      throw Exception('Error fetching sertifikasis: $e');
     }
   }
 }
