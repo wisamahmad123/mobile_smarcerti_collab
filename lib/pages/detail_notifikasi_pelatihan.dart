c67ffd85299694ec57bea628154ce0bddaf9057f
Shafirasal <shafirasalsabila15@gmail.com>
2024-11-26 17:56:59 +0700

    comment code

--- lib/pages/detail_notifikasi_pelatihan.dart
+++ lib/pages/detail_notifikasi_pelatihan.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_smarcerti/widgets/app_bar_custom.dart';
-import 'package:mobile_smarcerti/widgets/detail_notifikasi_pelatihan_body.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mobile_smarcerti/widgets/app_bar_custom.dart';
+// import 'package:mobile_smarcerti/widgets/detail_notifikasi_pelatihan_body.dart';
 
-class DetailNotifikasiPelatihan extends StatelessWidget {
-  const DetailNotifikasiPelatihan({super.key});
+// class DetailNotifikasiPelatihan extends StatelessWidget {
+//   const DetailNotifikasiPelatihan({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarCustom(
-        title: "Detail Notifikasi Pelatihan",
-      ),
-      body: DetailNotifikasiPelatihanBody(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBarCustom(
+//         title: "Detail Notifikasi Pelatihan",
+//       ),
+//       body: DetailNotifikasiPelatihanBody(),
+//     );
+//   }
+// }
--- lib/pages/pelatihan_page.dart
+++ lib/pages/pelatihan_page.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_smarcerti/widgets/app_bar_custom.dart';
-import 'package:mobile_smarcerti/widgets/dosen_bottom_navbar.dart';
-import 'package:mobile_smarcerti/widgets/pelatihan_body.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mobile_smarcerti/widgets/app_bar_custom.dart';
+// import 'package:mobile_smarcerti/widgets/dosen_bottom_navbar.dart';
+// import 'package:mobile_smarcerti/widgets/pelatihan_body.dart';
 
-class PelatihanPage extends StatelessWidget {
-  const PelatihanPage({super.key});
-  static final List<Tab> myTab = [
-    const Tab(
-      text: "All",
-    ),
-    const Tab(
-      text: "Status",
-    ),
-  ];
+// class PelatihanPage extends StatelessWidget {
+//   const PelatihanPage({super.key});
+//   static final List<Tab> myTab = [
+//     const Tab(
+//       text: "All",
+//     ),
+//     const Tab(
+//       text: "Status",
+//     ),
+//   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarCustom(
-          title: 'Daftar Pelatihan'), 
-      body: PelatihanBody(),
-      bottomNavigationBar: DosenBottomNavbar(currentIndex: -1),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBarCustom(
+//           title: 'Daftar Pelatihan'), 
+//       body: PelatihanBody(),
+//       bottomNavigationBar: DosenBottomNavbar(currentIndex: -1),
+//     );
+//   }
+// }
     
        
